1143fc47776577850d35a9519a41a77c86f783a7
Christopher Stokes <christopher.stokes@dealerspike.com>
2019-09-10 15:21:44 -0700

    probably overcomplicating thigns

--- minigamejam.js
+++ minigamejam.js
@@ -3,7 +3,11 @@
 // desc:   short description
 // script: js
 
-var t=0
+///////////////////////////////////////////////////////////////////
+// Do not alter this section, use these values for your basic game
+// I will comment wherever I can to explain how these fields work
+///////////////////////////////////////////////////////////////////
+
 var x=96
 var y=24
 var dx=0
@@ -11,19 +15,18 @@ var dy=0
 
 // base animation code
 function Animation(_frames, _timing, _width, _height, _ck) {
-	return {
-		"frames": _frames,
-		"timing": _timing,
-		"timeLast": time(),
-		"width": _width || 8,
-		"height": _height || 8,
-		"currentFrame": 0,
-		"ck": _ck || 0
-	}
+	this.frames = _frames
+	this.timing = _timing
+	this.timeLast = time()
+	this.width = _width || 8
+	this.height = _height || 8
+	this.currentFrame = 0
+	this.ck = _ck || 0
 }
 
 Animation.prototype.update = function() {
-	if (time() - this.timeLast() > this.timing) {
+	if (time() - this.timeLast >= this.timing) {
+		this.timeLast = time()
 		if (this.currentFrame < this.frames.length) {
 			this.currentFrame+=1
 		} else {
@@ -38,34 +41,66 @@ Animation.prototype.draw = function(_x, _y, _flp, _rot) {
 }
 
 // base actor code
-function Actor() {
+function Actor(_x, _y, _animations, _hb) {
+	this.x = _x
+	this.y = _y
+	this.dx = 0
+	this.dy = 0
+	this.flp = 0
+	this.rot = 0
+	this.animations = _animations || []
+	this.currentAnimation = "default"
+	this.hb = _hb || {}
+}
 
+Actor.prototype.addAnim = function(_name, _anim) {
+	Actor.animations.push({
+		"name": _name,
+		"anim": _anim
+	})
 }
 
 Actor.prototype.update = function() {
-
+	this.x += this.dx
+	this.y += this.dy
 }
 
 Actor.prototype.draw = function() {
-
+	this.animations[this.currentAnimation].draw(this.x, this.y, this.flp, this.rot)
 }
 
-// base state code
-function State() {
-
+// individual game config -- please update these values
+var gameConfig={
+	"game-title": "[TITLE OF YOUR GAME]",
+	"game-author": "[YOUR NAME OR THE NAMES OF YOUR TEAM SEPARATED BY A COMMA]",
 }
 
-function TIC()
-{
+/////////////////////////////////////////
+// End of template code
+// Your code should go after this point
+////////////////////////////////////////
+
+var MiniGame = {}
+
+// this is the game  loop you will be using
+// all code that needs to be run each frame will need
+// to go here
+
+MiniGame.update = function() {
 	if(btn(0))y--
 	if(btn(1))y++
 	if(btn(2))x--
 	if(btn(3))x++
 
 	cls(13)
-	spr(1+((t%60)/30|0)*2,x,y,14,3,0,0,2,2)
-	print("HELLO WORLD!",84,84)
-	t++
+	spr(1,x,y,14,3,0,0,2,2)
+}
+
+
+
+function TIC()
+{
+	MiniGame.update()
 }
 
 // <TILES>
